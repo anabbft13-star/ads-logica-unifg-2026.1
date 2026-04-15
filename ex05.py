e5377a2b0e6e3367e02cae467ed33efb7dc19f7c
anabbft13-star <anabbft13@gmail.com>
2026-04-15 19:23:18 -0300

    Implement positive number counter in ex05.py
    Fix indentation and syntax errors in the script.

---
+++ ex05.py
@@ -0,0 +1,6 @@
+contador_positivos=0
+for in range(10):
+numero=float(input("digite um numero:"))
+      if numero>0:
+      contador_positivos+=1
+print("quantidade de numeros positivos:",contador_positivos)
